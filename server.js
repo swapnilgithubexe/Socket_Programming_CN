49ef10a8eb4b7abf06673fe66b9f2f370502bcbf
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-02-08 02:38:17 +0530

    updates

--- server.js
+++ server.js
@@ -19,6 +19,19 @@ const io = new Server(server, {
 //socket events
 io.on("connection", (socket) => {
   console.log("Socket sonnection established");
+
+  socket.on("join", (data) => {
+    socket.username = data;
+  })
+  socket.on("new_message", (message) => {
+
+    let userMessage = {
+      username: socket.username,
+      message: message
+    }
+    //broadcast message
+    socket.broadcast.emit("broadcast_message", userMessage);
+  });
   socket.on("disconnect", () => {
     console.log("Socket connection disconnected");
 
